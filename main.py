d644a531cdee37ca10a7a355eb4cdbc3c45fcaac
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-02-10 03:05:25 +0530

    added convolution layers and pass the dataset to it

--- main.py
+++ main.py
@@ -43,3 +43,32 @@ test_loader = DataLoader(
 images, labels = next(iter(train_loader))
 print(images.shape, labels.shape)
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FashionCNN(nn.Module):
+    def __init__(self):
+        super(FashionCNN, self).__init__()
+
+        # Convolution layers
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+
+        # Pooling
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Fully connected layers
+        self.fc1 = nn.Linear(64 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        x = x.view(x.size(0), -1)  # flatten
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
